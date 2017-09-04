b9e7fe1002873841f81fbb223e0c3ff5538a0010
czd890 <czd890@live.cn>
2017-09-04 18:38:10 +0800

    remove test dir

--- bin-test/test/apiclienttest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ApiClient_1 = require("../src/ApiClient");
-var client = new ApiClient_1.ApiClient();
-client.Get('https://www.baidu.com').then(function (res) {
-    console.log(res);
-});
-//# sourceMappingURL=apiclienttest.js.map
