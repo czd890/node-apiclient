7c7629027c305cf191be576c7b89b2833e9cd9e4
czd890 <czd890@live.cn>
2017-09-04 19:00:42 +0800

    add Bluebird test

--- test/apiclienttest.ts
+++ test/apiclienttest.ts
@@ -1,6 +1,21 @@
 import { ApiClient } from '../src/ApiClient';
-
+import * as Bluebird from 'bluebird'
 var client = new ApiClient();
-client.Get('https://www.baidu.com').then(res => {
-    console.log(res)
+var baiduReq = client.Get('https://www.baidu.com').then(res => {
+    // console.log(res)
+    return res;
 })
+
+var taobaoReq = client.Get("https://www.taobao.com");
+
+
+Bluebird.props({
+    baidu_res: baiduReq,
+    taobao_res: taobaoReq
+}).then(data => {
+    console.log('baidu 返回')
+    console.log(data.baidu_res)
+    console.log('淘宝返回 返回')
+    console.log(data.taobao_res)
+})
+
