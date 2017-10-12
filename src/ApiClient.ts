8023b1ebfb10e708d212972d48cf4822cdcdcbce
czd890 <czd890@live.cn>
2017-10-12 21:15:14 +0800

    自动附加traceid

--- src/ApiClient.ts
+++ src/ApiClient.ts
@@ -1,7 +1,8 @@
 import * as request from 'request';
 import { CoreOptions, UrlOptions, RequestResponse } from 'request';
+import * as core from "express-serve-static-core";
 import * as url from 'url';
-
+import * as localStorage from 'continuation-local-storage'
 import { Options } from "./Options";
 
 // var tc = require('../lib/timeConsuming');
@@ -17,7 +18,13 @@ export class ApiClient {
         options.timeout = options.timeout || 3000;
         options.postTimeout = options.postTimeout || 5000;
         options.querystring = options.querystring || '';
+        options.headers = options.headers || {};
         options.keepAlive = options.keepAlive === false ? false : true;
+
+        var namespace = localStorage.getNamespace('gd-api-client-local-storage')
+        if (namespace)
+            options.headers['X-Request-Id'] = namespace.get('X-Request-Id')
+
         return options;
     }
     Get<TData>(host: string, path?: string, params?: any, options?: Options): Promise<TData> {
@@ -45,6 +52,7 @@ export class ApiClient {
     BasicRequest(options: Options): Promise<RequestResponse> {
         var opt: CoreOptions & UrlOptions = { url: '' }
         opt.timeout = options.timeout
+        opt.forever = options.keepAlive
         opt.url = url.resolve(options.host || '', options.path || '')
 
         if (options.querystring) {
@@ -60,10 +68,15 @@ export class ApiClient {
             else if (options.headers && (options.headers['Content-Type'] == 'multipart/form-data' || options.headers['content-type'] == 'multipart/form-data')) {
                 opt.formData = options.data;
             }
-            else
+            else {
+                if (!options.headers["Content-Type"] && !options.headers["content-type"])
+                    options.headers["content-type"] = "application/json";
                 opt.body = JSON.stringify(options.data);
+            }
             if (options.postTimeout) opt.timeout = options.postTimeout
         }
+
+        opt.headers = options.headers;
         return new Promise((resolve, reject) => {
             var req = request(opt, (error: any, response: RequestResponse, body: any) => {
                 if (error) {
@@ -74,4 +87,12 @@ export class ApiClient {
             });
         });
     }
+}
+
+export function UseApiClient(req: core.Request, res: core.Response, next: core.NextFunction) {
+    var namespace = localStorage.createNamespace('gd-api-client-local-storage');
+    namespace.run(() => {
+        namespace.set('X-Request-Id', req.header('X-Request-Id'))
+        next && next()
+    });
 }
